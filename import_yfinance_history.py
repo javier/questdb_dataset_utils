fecaaa8fb7212f752e3ca517e0043c6109a12274
javier <javier@formatinternet.com>
2023-09-05 19:31:47 +0200

    changed timestamp filter

--- import_yfinance_history.py
+++ import_yfinance_history.py
@@ -2,13 +2,47 @@ import yfinance as yf
 import pandas as pd
 from questdb.ingress import Sender, IngressError
 import sys
+import requests
+import json
+
+def run_query(host, sql_query):
+    query_params = {'query': sql_query, 'fmt' : 'json'}
+    try:
+        response = requests.get(host + '/exec', params=query_params)
+        json_response = json.loads(response.text)
+        #print(json_response)
+    except requests.exceptions.RequestException as e:
+        print(f'Error: {e}')
+
+def create_table(host):
+     run_query(host,
+        """
+            CREATE TABLE IF NOT EXIST 'nasdaq' (
+            Ticker SYMBOL capacity 256 CACHE,
+            Open DOUBLE,
+            High DOUBLE,
+            Low DOUBLE,
+            Close DOUBLE,
+            Adj Close DOUBLE,
+            Volume LONG,
+            Timestamp TIMESTAMP
+    ) timestamp (Timestamp) PARTITION BY MONTH WAL;
+    """
+     )
+
+     run_query(host,
+        """
+            TRUNCATE TABLE 'nasdaq' (
+        """
+     )
+
 
 def get_tickers(tickerStrings, start, end):
     df_list = list()
 
     for ticker in tickerStrings:
         data = yf.download(ticker, group_by="Ticker", start=start, end=end)
-        data['ticker'] = ticker  # add this column because the dataframe doesn't contain a column with the ticker
+        data['Ticker'] = ticker  # add this column because the dataframe doesn't contain a column with the ticker
         df_list.append(data)
         # combine all dataframes into a single dataframe
         df = pd.concat(df_list)
@@ -21,11 +55,12 @@ def get_tickers(tickerStrings, start, end):
 def write_table(df, table_name, host, port):
     try:
         with Sender(host, port) as sender:
-            sender.dataframe(df, table_name=table_name, symbols=['ticker'], at='Date')
+            sender.dataframe(df, table_name=table_name, symbols=['Ticker'], at='Date')
     except IngressError as e:
             sys.stderr.write(f'Got error: {e}\n')
 
 if __name__ == '__main__':
+     create_table('http://localhost:9000')
      tickerStrings = ['TSLA', 'NVDA', 'AMD', 'AVGO', 'AMZN', 'META', 'GOOGL', 'AAPL', 'MSFT']
      start = '2017-09-01'
      end = '2023-09-05'
